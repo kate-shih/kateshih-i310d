acf9874b4d4758bc59278b38719cf3ffd584016d
Katie Shih <katie@wireless-10-148-115-200.public.utexas.edu>
2024-04-12 13:22:25 -0500

    added volume_of_sphere.py

---
+++ volume_of_sphere.py
@@ -0,0 +1,11 @@
+import math
+
+def calculate_volume_of_sphere(radius):
+    volume = (4/3) * math.pi * radius**3
+    return volume
+
+radius1 = 30
+volume1 = calculate_volume_of_sphere(radius1)
+
+radius2 = 40
+volume2 = calculate_volume_of_sphere(radius2)
